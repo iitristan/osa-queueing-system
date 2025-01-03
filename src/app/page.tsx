3a0585c3452faa99db2d473e38ab71abd9550f1d
tristan <88244585+iitristan@users.noreply.github.com>
2025-01-04 01:50:31 +0800

    first edition

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,26 +13,20 @@ export default function Home() {
           className="p-3"
         />
       </nav>
-      <div className="grid grid-cols-2 grid-row-3">
-        <div>
-          <div className="text-center py-20 bg-[#D0D0D0] m-7">
-            <h1 className="text-4xl font-bold">Now Serving</h1>
-            <h1 className="text-9xl font-bold">S07</h1>
-            <h1 className="text-4xl font-bold">Proceed to [Latest Counter]</h1>
-          </div>
-          <div className="text-center py-20 bg-[#D0D0D0] m-7">
-            <h1 className="text-4xl font-bold">Now Serving</h1>
-            <h1 className="text-9xl font-bold">S07</h1>
-            <h1 className="text-4xl font-bold">Proceed to [Latest Counter]</h1>
-          </div>
+      <div className="grid grid-rows-4 grid-flow-col gap-2 m-2">
+        <div className="row-span-4 content-center text-center bg-[#D0D0D0]">
+          <h1 className="text-4xl font-bold">Now Serving</h1>
+          <h1 className="text-9xl font-bold">S07</h1>
+          <h1 className="text-4xl font-bold">Proceed to [Latest Counter]</h1>
         </div>
 
-        <div className="text-center">
-          <div className="text-center py-20 bg-[#D0D0D0] m-7">s</div>
-          <div className="text-center py-20 bg-[#D0D0D0] m-7">s</div>
-          <div className="text-center py-20 bg-[#D0D0D0] m-7">s</div>
-          <div className="text-center py-20 bg-[#D0D0D0] m-7">s</div>
+        <div className="text-center content-center row-span-1 py-5 bg-[#D0D0D0] ">
+          <h1 className="text-4xl font-bold">Counter</h1>
+          <h1 className="text-4xl font-bold">Queue Number</h1>
         </div>
+        <div className="text-center row-span-1 py-20 bg-[#D0D0D0] ">s</div>
+        <div className="text-center row-span-1 py-20 bg-[#D0D0D0] ">s</div>
+        <div className="text-center row-span-1 py-20 bg-[#D0D0D0] ">s</div>
       </div>
     </div>
   );
