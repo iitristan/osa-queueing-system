fa3446013f50c5ddd8ec3c2676d8b00d638df167
tristan <88244585+iitristan@users.noreply.github.com>
2025-01-04 02:24:42 +0800

    final iteration for front

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,8 @@
 import Image from "next/image";
-import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="">
+    <div className="h-[100dvh] overflow-hidden">
       <nav className="bg-[#111111] border-[#FCBF15] border-b-4">
         <Image
           src="/osa_header.png"
@@ -13,20 +12,24 @@ export default function Home() {
           className="p-3"
         />
       </nav>
-      <div className="grid grid-rows-4 grid-flow-col gap-2 m-2">
-        <div className="row-span-4 content-center text-center bg-[#D0D0D0]">
+      <div className="grid grid-cols-2 gap-1 h-full">
+        <div className="flex flex-col justify-center items-center bg-[#D0D0D0]">
           <h1 className="text-4xl font-bold">Now Serving</h1>
           <h1 className="text-9xl font-bold">S07</h1>
           <h1 className="text-4xl font-bold">Proceed to [Latest Counter]</h1>
         </div>
 
-        <div className="text-center content-center row-span-1 py-5 bg-[#D0D0D0] ">
-          <h1 className="text-4xl font-bold">Counter</h1>
-          <h1 className="text-4xl font-bold">Queue Number</h1>
+        <div className="flex grid-cols-*  bg-[#D0D0D0] ">
+          <div className="border-r-2 px-10 py-4 items-center">
+            <h1 className="text-4xl font-bold">Table</h1>
+          </div>
+          <div className="border-r-2 px-10 py-4 items-center">
+            <h1 className="text-4xl font-bold">OSA Staff</h1>
+          </div>
+          <div className="px-10 py-4 items-center">
+            <h1 className="text-4xl font-bold">Queue Number</h1>
+          </div>
         </div>
-        <div className="text-center row-span-1 py-20 bg-[#D0D0D0] ">s</div>
-        <div className="text-center row-span-1 py-20 bg-[#D0D0D0] ">s</div>
-        <div className="text-center row-span-1 py-20 bg-[#D0D0D0] ">s</div>
       </div>
     </div>
   );
